6cc6730fbef643e755d59110d7cd19b19a332543
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2023-01-03 09:53:34 +0700

    up

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ const rabbitConfig = {
     uri: process.env.rabbitUri || 'amqp://localhost',
     workQueue: process.env.workQueue || 'js.ticket',
     key: 'create.one',
-}
+} 
 
 export default rabbitConfig
